4555d7681d03243af583ec0b43b14a5e85ff67ae
Miha77777 <lv11miha@gmail.com>
2023-09-28 21:50:15 +0300

    added disabled btn

--- web/js/main.js
+++ web/js/main.js
@@ -1,5 +1,15 @@
+var input = document.querySelector(".weather-form-input")
+
 async function getWeather() {
-	var input = document.querySelector(".weather-form-input").value;
-	var result = await eel.get_weather(input)();
+	var inputValue = document.querySelector(".weather-form-input").value;
+	var result = await eel.get_weather(inputValue)();
 	document.querySelector(".second").textContent = result;
 }
+
+input.addEventListener("input", () => {
+	if (document.querySelector(".weather-form-input").value) {
+		document.querySelector(".weather-form-btn").disabled = false;
+	} else {
+		document.querySelector(".weather-form-btn").disabled = true;
+	}
+})
